d5236b0f650f5b01c848d8dee785f8cba2d359c2
Reacher <Reacher@ReacherdeMacBook-Pro.local>
2015-06-25 14:07:28 +0800

    小修正

--- app/boot.js
+++ app/boot.js
@@ -2,7 +2,6 @@
 
 import MainApp from './views/MainApp.jsx';
 var App = React.createFactory( MainApp);
-// var MainApp = React.createFactory(require('./MainApp.jsx'));
 
 $(function(){
   React.render( App(), document.getElementById('app'));
